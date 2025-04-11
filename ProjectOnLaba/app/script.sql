64754927b13d7c48c0dfe5baa1c5c513ad852569
VioNo <118349098+VioNo@users.noreply.github.com>
2025-04-11 20:09:03 +0300

    Update script.sql

--- ProjectOnLaba/app/script.sql
+++ ProjectOnLaba/app/script.sql
@@ -1,4 +1,92 @@
--- Создание последовательности для users
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.4 (Debian 17.4-1.pgdg120+2)
+-- Dumped by pg_dump version 17.4 (Debian 17.4-1.pgdg120+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+
+
+CREATE TABLE public.registration (
+    user_id integer NOT NULL,
+    login character varying(255) NOT NULL,
+    password character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.registration OWNER TO postgres;
+
+
+
+CREATE SEQUENCE public.tasks_id_seq_new
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.tasks_id_seq_new OWNER TO postgres;
+
+
+
+CREATE TABLE public.tasks (
+    id integer DEFAULT nextval('public.tasks_id_seq_new'::regclass) NOT NULL,
+    title character varying(255) NOT NULL,
+    description text,
+    status character varying(50) NOT NULL,
+    user_id integer NOT NULL,
+    create_at date DEFAULT CURRENT_DATE NOT NULL
+);
+
+
+ALTER TABLE public.tasks OWNER TO postgres;
+
+
+CREATE SEQUENCE public.tasks_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.tasks_id_seq OWNER TO postgres;
+
+
+
+ALTER SEQUENCE public.tasks_id_seq OWNED BY public.tasks.id;
+
+
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    login character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    password_hash character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+
 CREATE SEQUENCE public.users_id_seq
     AS integer
     START WITH 1
@@ -7,41 +95,51 @@ CREATE SEQUENCE public.users_id_seq
     NO MAXVALUE
     CACHE 1;
 
--- Создание таблицы users
-CREATE TABLE public.users (
-    id integer DEFAULT nextval('public.users_id_seq'::regclass) NOT NULL,
-    login character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    password_hash character varying(255) NOT NULL
-);
 
--- Создание таблицы tasks (с автоинкрементом через IDENTITY)
-CREATE TABLE public.tasks (
-    id integer GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL,
-    title character varying(255) NOT NULL,
-    description text,
-    status character varying(50) NOT NULL,
-    user_id integer NOT NULL,
-    create_at date DEFAULT CURRENT_DATE NOT NULL
-);
+ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
--- Установка текущего значения последовательности для users
-SELECT pg_catalog.setval('public.users_id_seq', 61, true);
 
--- Добавление ограничений для users
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+
+ALTER TABLE ONLY public.registration
+    ADD CONSTRAINT registration_pkey PRIMARY KEY (user_id);
+
+
+
+
+ALTER TABLE ONLY public.tasks
+    ADD CONSTRAINT tasks_pkey PRIMARY KEY (id);
+
+
+
+
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_login_key UNIQUE (login);
 
+
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
--- Добавление ограничений для tasks
-ALTER TABLE ONLY public.tasks
-    ADD CONSTRAINT tasks_pkey PRIMARY KEY (id);
 
--- Добавление внешнего ключа для связи tasks с users
+
+ALTER TABLE ONLY public.registration
+    ADD CONSTRAINT registration_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+
 ALTER TABLE ONLY public.tasks
     ADD CONSTRAINT tasks_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
